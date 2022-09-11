d5448c8c65fe9ab101606bdf0c614d339eea0757
jay-brew <brew0@naver.com>
2022-09-11 20:15:54 +0900

    redux toolkit 이용

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,9 +1,12 @@
+import {productActions} from "../reducers/productReducer";
+
 function getProducts(keyword){
     return async(dispatch, getState) => {
         let url = `https://my-json-server.typicode.com/jay-brew/miniProject/products?q=${keyword}`;
         let response = await fetch(url);
         let data = await response.json();
-        dispatch({type:"GET_PRODUCT_SUCCESS",payload:{data}});
+        // dispatch({type:"GET_PRODUCT_SUCCESS",payload:{data}});
+        dispatch(productActions.getAllProducts({data}));
     };
 };
 
@@ -12,7 +15,8 @@ function getProductDetail(id) {
         let url = `https://my-json-server.typicode.com/jay-brew/miniProject/products/${id}`
         let response = await fetch(url);
         let data = await response.json();
-        dispatch({type:"GET_SINGLE_PRODUCT_SUCCESS",payload:{data}});
+        //dispatch({type:"GET_SINGLE_PRODUCT_SUCCESS",payload:{data}});
+        dispatch(productActions.getSingleProduct({data}));
     };
 };
 
